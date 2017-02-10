a5bddffeec955e3b1ff4991f8a434dc4952876c6
James M <jwmarion@gmail.com>
2017-02-10 11:35:41 -0500

    changed temp range and removed unused code from tempConvert

--- tempConvert.py
+++ tempConvert.py
@@ -1,15 +1,14 @@
 #tempConvert.py
 
-from numpy import arange
+
 import matplotlib.pyplot as plot
-import math
+
 temp = int(raw_input("Enter a temp in celcius: "))
 
 def f(x):
     return (x * 1.8) + 32
 
-
-xs = arange(temp -10, temp + 10, 0.5)
+xs = range(temp -5, temp + 5, 1)
 ys = []
 for x in xs:
     ys.append(f(x))
