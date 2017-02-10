6950f28276826dd24d1e2b91f510a46ce4ebbbe7
James M <jwmarion@gmail.com>
2017-02-10 11:21:50 -0500

    added offOrEven.py

---
+++ oddOrEven.py
@@ -0,0 +1,17 @@
+#oddOrEven
+
+import matplotlib.pyplot as plot
+
+def f(x):
+    if x % 2 == 0:
+        return -1
+    else:
+        return 1
+
+xs = range(-5, 5)
+ys = []
+for x in xs:
+    ys.append(f(x))
+
+plot.bar(xs, ys)
+plot.show()
