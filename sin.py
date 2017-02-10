abfbbe30061d17d0b0c9881dc343ecd1b7c32ad4
James M <jwmarion@gmail.com>
2017-02-10 11:22:05 -0500

    added sin.py

---
+++ sin.py
@@ -0,0 +1,16 @@
+#sin.py
+
+import matplotlib.pyplot as plot
+import math
+
+def f(x):
+    return math.sin(x)
+
+
+xs = range(-5, 5)
+ys = []
+for x in xs:
+    ys.append(f(x))
+
+plot.plot(xs, ys)
+plot.show()
